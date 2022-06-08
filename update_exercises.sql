58535fa75f6f3ca508748726151b3708199eaf96
Stout13 <stout.stephen13@gmail.com>
2022-06-08 08:51:21 -0500

    update exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,5 +1,8 @@
 USE codeup_test_db;
 
-
+SELECT 'all the things' AS 'flip-flaps';
 SELECT * FROM codeup_test_db.albums;
-SELECT * FROM codeup_test_db.albums;
+SELECT  'all the albums released before 1980' AS 'classics';
+SELECT * FROM codeup_test_db.albums WHERE albums.releaseDate < 1980;
+SELECT 'bad thriller' AS 'dangerous';
+SELECT * FROM codeup_test_db.albums WHERE albums.artist = 'Michael Jackson';
