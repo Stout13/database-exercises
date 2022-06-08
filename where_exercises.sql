04eec83f34cc940f2f4212f5d6e39c5207826760
Stout13 <stout.stephen13@gmail.com>
2022-06-08 13:51:21 -0500

    where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,4 +5,30 @@
 
 USE employees;
 
-SELECT * FROM employees WHERE employees.employees.first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE (employees.employees.first_name LIKE ('Irena')
+                            OR employees.employees.first_name LIKE ('Vidya')
+                            OR employees.employees.first_name LIKE ('Maya')
+                            OR employees.employees.last_name LIKE ('E%')
+                            OR employees.employees.last_name LIKE ('%q%'))
+                            AND employees.employees.gender IN ('M');
+
+SELECT * FROM employees WHERE (employees.employees.first_name LIKE ('Irena')
+    OR employees.employees.first_name LIKE ('Vidya')
+    OR employees.employees.first_name LIKE ('Maya')
+    OR employees.employees.last_name LIKE ('%q%'))
+#     OR employees.employees.last_name LIKE ('%Q%'))
+                            AND employees.employees.gender IN ('M')
+                            AND employees.employees.last_name LIKE ('E%')
+                            AND employees.employees.last_name LIKE ('%E')
+                            AND NOT employees.employees.last_name LIKE ('%qu%');
+
+
+# Duplicate the previous query and update it to find all employees whose last name
+# starts and ends with 'E'
+
+
+# SELECT * FROM employees WHERE employees.employees.last_name LIKE ('E%');
+#
+# SELECT * FROM employees WHERE (employees.employees.last_name LIKE ('%q%') OR employees.employees.last_name LIKE ('%Q%'));
+#
+# SELECT * FROM employees WHERE (employees.employees.last_name) LIKE ()
