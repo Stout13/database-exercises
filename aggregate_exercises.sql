ae43e82c2f85e34d618ab6d38df74a52f61d669d
Stout13 <stout.stephen13@gmail.com>
2022-06-09 16:35:09 -0500

    aggregate_exercises

--- aggregate_exercises.sql
+++ aggregate_exercises.sql
@@ -3,10 +3,15 @@ USE join_test_db;
 # select count of role_id from users
 # select count(*) as users from users
 
-SELECT COUNT(role_id), r.role_id FROM users JOIN users r on roles. = r.id GROUP BY users.ID;
+SELECT COUNT(*), roles.name FROM roles JOIN users ON roles.id = users.role_id GROUP BY roles.name;
+# JOIN roles ON users.role_id GROUP BY
+# SELECT COUNT(*), roles.name FROM roles GROUP BY roles.name;
+
+
+
 
 #     JOIN users u ON roles.id = u.role_id GROUP BY role_id;
-SELECT roles.name FROM roles JOIN users u on roles.id = u.role_id GROUP BY roles.name;
-SELECT COUNT(role_id), role_id FROM users JOIN roles r on users.role_id = r.id GROUP BY role_id;
+# SELECT roles.name FROM roles JOIN users u on roles.id = u.role_id GROUP BY roles.name;
+# SELECT COUNT(role_id), role_id FROM users JOIN roles r on users.role_id = r.id GROUP BY role_id;
 
 
