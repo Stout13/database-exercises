4c1d3d931d81c42697c83cf3f64c11679d84a079
Stout13 <stout.stephen13@gmail.com>
2022-06-13 21:36:38 -0500

    order_by_exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,34 @@
+# Create a file named where_exercises.sql. Make sure to use the employees database before you begin.
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+# Find all employees whose last name starts with 'E' — 7,330 rows.
+# Find all employees with a 'q' in their last name — 1,873 rows.
+
+USE employees;
+
+SELECT * FROM employees WHERE (employees.employees.first_name LIKE ('Irena')
+    OR employees.employees.first_name LIKE ('Vidya')
+    OR employees.employees.first_name LIKE ('Maya')
+    OR employees.employees.last_name LIKE ('E%')
+    OR employees.employees.last_name LIKE ('%q%'))
+                          AND employees.employees.gender IN ('M');
+
+SELECT * FROM employees WHERE (employees.employees.first_name LIKE ('Irena')
+    OR employees.employees.first_name LIKE ('Vidya')
+    OR employees.employees.first_name LIKE ('Maya')
+    OR employees.employees.last_name LIKE ('%q%'))
+#     OR employees.employees.last_name LIKE ('%Q%'))
+                          AND employees.employees.gender IN ('M')
+                          AND employees.employees.last_name LIKE ('E%')
+                          AND employees.employees.last_name LIKE ('%E')
+                          AND NOT employees.employees.last_name LIKE ('%qu%');
+
+
+# Duplicate the previous query and update it to find all employees whose last name
+# starts and ends with 'E'
+
+
+# SELECT * FROM employees WHERE employees.employees.last_name LIKE ('E%');
+#
+# SELECT * FROM employees WHERE (employees.employees.last_name LIKE ('%q%') OR employees.employees.last_name LIKE ('%Q%'));
+#
+# SELECT * FROM employees WHERE (employees.employees.last_name) LIKE ()
