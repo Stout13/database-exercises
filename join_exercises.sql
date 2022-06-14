d74f48f1f85142462b55cef4a6f6418842ad1068
Stout13 <stout.stephen13@gmail.com>
2022-06-13 20:52:51 -0500

    databases-exercises

---
+++ join_exercises.sql
@@ -0,0 +1,85 @@
+# Create a file named join_exercises.sql to do your work in.
+#
+# Using the example in the Associative Table Joins section as a guide,
+# write a query that shows each department along with the name of the current manager
+# for that department.
+#
+
+
+
+# SELECT * FROM titles;
+# SELECT CONCAT(employees.last_name, ', ', employees.first_name) FROM employees;
+
+SELECT departments.dept_name, CONCAT(employees.last_name, ', ', employees.first_name) AS name FROM employees JOIN dept_emp on  employees.emp_no = dept_emp.emp_no JOIN departments ON departments.dept_no = dept_emp.dept_no JOIN dept_manager ON departments.dept_no = dept_manager.dept_no JOIN titles ON titles.emp_no = employees.emp_no WHERE (titles.title IN ('Manager') AND NOW() < titles.to_date);
+
+# SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+# FROM employees as e
+#          JOIN dept_manager as de
+#               ON de.emp_no = e.emp_no
+#          JOIN departments as d
+#               ON d.dept_no = de.dept_no;
+
+#
+# +--------------------+--------------------+
+# | Department Name    | Department Manager |
+# +--------------------+--------------------+
+# | Customer Service   | Yuchang Weedman    |
+# | Development        | Leon DasSarma      |
+# | Finance            | Isamu Legleitner   |
+# | Human Resources    | Karsten Sigstam    |
+# | Marketing          | Vishwani Minakawa  |
+# | Production         | Oscar Ghazalie     |
+# | Quality Management | Dung Pesch         |
+# | Research           | Hilary Kambil      |
+# | Sales              | Hauke Zhang        |
+# +--------------------+--------------------+
+# Find the name of all departments currently managed by women.
+#
+# +------------------+--------------------+
+# | Department Name  | Department Manager |
+# +------------------+--------------------+
+# | Development      | Leon DasSarma      |
+# | Finance          | Isamu Legleitner   |
+# | Human Resources  | Karsten Sigstam    |
+# | Research         | Hilary Kambil      |
+# +------------------+--------------------+
+# Find the current titles of employees currently working in the Customer Service department.
+#
+# +--------------------+-------+
+# | title              | Total |
+# +--------------------+-------+
+# | Senior Staff       | 11268 |
+# | Staff              |  3574 |
+# | Senior Engineer    |  1790 |
+# | Engineer           |   627 |
+# | Technique Leader   |   241 |
+# | Assistant Engineer |    68 |
+# | Manager            |     1 |
+# +--------------------+-------+
+# Find the current salary of all current managers.
+#
+# +--------------------+--------------------+--------+
+# | Department Name    | Department Manager | Salary |
+# +--------------------+--------------------+--------+
+# | Customer Service   | Yuchang Weedman    |  58745 |
+# | Development        | Leon DasSarma      |  74510 |
+# | Finance            | Isamu Legleitner   |  83457 |
+# | Human Resources    | Karsten Sigstam    |  65400 |
+# | Marketing          | Vishwani Minakawa  | 106491 |
+# | Production         | Oscar Ghazalie     |  56654 |
+# | Quality Management | Dung Pesch         |  72876 |
+# | Research           | Hilary Kambil      |  79393 |
+# | Sales              | Hauke Zhang        | 101987 |
+# +--------------------+--------------------+--------+
+# Bonus Find the names of all current employees, their department name, and their current manager's name .
+#
+#
+# +----------------------+------------------+-----------------+
+# | Employee             | Department       | Manager         |
+# +----------------------+------------------+-----------------+
+# | Huan Lortz           | Customer Service | Yuchang Weedman |
+# | Basil Tramer         | Customer Service | Yuchang Weedman |
+# | Breannda Billingsley | Customer Service | Yuchang Weedman |
+# | Jungsoon Syrzycki    | Customer Service | Yuchang Weedman |
+# | Yuichiro Swick       | Customer Service | Yuchang Weedman |
+# ... 240,124 Rows in total
