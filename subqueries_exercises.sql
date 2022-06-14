7e35fa4ecf77278ef0de0499b0ed3d21c40f2e79
Stout13 <stout.stephen13@gmail.com>
2022-06-13 21:22:40 -0500

    subqueries_exercises final

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 
-SELECT employees.employees.hire_date FROM employees WHERE (employees.employees.emp_no = 101010);
+# SELECT employees.employees.hire_date FROM employees WHERE (employees.employees.emp_no = 101010);
 
 SELECT CONCAT(employees.employees.last_name, ', ', employees.employees.first_name) AS name FROM employees WHERE (employees.employees.hire_date = '1990-10-22');
 # Find all the employees with the same hire date as employee 101010 using a subquery.
@@ -9,6 +9,21 @@ SELECT CONCAT(employees.employees.last_name, ', ', employees.employees.first_nam
 #
 # Find all the titles held by all employees with the first name Aamod.
 #
+
+SELECT titles.title, CONCAT(last_name, ', ', first_name) FROM titles JOIN employees as e on titles.emp_no = e.emp_no WHERE (first_name IN ('Aamod'));
+
+
 # 314 total titles, 6 unique titles
 #
 # Find all the current department managers that are female.
+
+SELECT CONCAT(last_name, ', ', first_name) AS NAME, gender FROM employees WHERE employees.gender = 'F';
+#
+# +------------+------------+
+# | first_name | last_name  |
+# +------------+------------+
+# | Isamu      | Legleitner |
+# | Karsten    | Sigstam    |
+# | Leon       | DasSarma   |
+# | Hilary     | Kambil     |
+# +------------+------------+
